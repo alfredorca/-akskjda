583e96550587db23b090ed985e579b10f1dd0d20
alfredorca <alfredorca@gmail.com>
2021-09-25 17:26:01 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 //Import functionality
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import React, { useState, useEffect } from "react";
 import "./App.css";
 import SideBar from "./Components/SideBar";
-import axios from "axios";
-import Coin from "./Views/Coin";
 import "./App.css";
 import AboutUsView from "./Views/AboutUsView";
 
@@ -12,29 +9,6 @@ import AboutUsView from "./Views/AboutUsView";
 import HomeView from "./Views/HomeView";
 
 function App() {
-  const [coins, setCoins] = useState([]);
-  const [search, setSearch] = useState("");
-
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false"
-      )
-      .then((res) => {
-        setCoins(res.data);
-        console.log(res.data);
-      })
-      .catch((error) => alert("error"));
-  }, []);
-
-  const handleChange = (e) => {
-    setSearch(e.target.value);
-  };
-
-  const filteredCoins = coins.filter((coin) =>
-    coin.name.toLowerCase().includes(search.toLowerCase())
-  );
-
   return (
     <>
       <SideBar />
@@ -42,7 +16,6 @@ function App() {
         <Switch>
           <Route exact path="/" component={HomeView} />
           <Route exact path="/aboutus" component={AboutUsView} />
-          {/* <Route exact path = "/" component={HomeView}/> */}
         </Switch>
       </BrowserRouter>
     </>
--- src/Views/AboutUsView.js
+++ src/Views/AboutUsView.js
@@ -1,20 +1,28 @@
 import { height } from "@mui/system";
 import { AboutUsData } from "../Components/AboutUsData";
-let alfredosPicture = '../images/Alfredo.jpg';
+let alfredosPicture = "../images/Alfredo.jpg";
 
 const AboutUsView = () => {
   return (
     <section className="container">
       <div className="persons">
-      {AboutUsData.map(person => (
-        <div className="singlePerson">
-          <div className="picture" style={{background: `url('${person.picture}')`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', height:'400px'}}> 
+        {AboutUsData.map((person) => (
+          <div className="singlePerson">
+            <div
+              className="picture"
+              style={{
+                background: `url('${person.picture}')`,
+                backgroundSize: "contain",
+                backgroundRepeat: "no-repeat",
+                height: "80vh",
+              }}
+            ></div>
+            <div className="name">{person.name}</div>
           </div>
-        </div>
-      ))}
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default AboutUsView;
