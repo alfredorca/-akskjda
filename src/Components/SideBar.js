a2a8847900dbd51f144f5ad682f2955aee7da6b1
alfredorca <alfredorca@gmail.com>
2021-09-25 21:38:03 -0400

    Added Sherk

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -3,22 +3,35 @@ import { SideBarData } from "./SideBarData";
 const SideBar = () => {
   return (
     <div className="sidebar">
-      <ul>
-        {SideBarData.map((val, key) => (
-          <li
-            key={key}
-            onClick={() => {
-              window.location.pathname = val.link;
-            }}
-            id={window.location.pathname === val.link ? "active" : ""}
-          >
-            <div id="icon">{val.icon}</div>
-            <div id="title">{val.title}</div>
-          </li>
-        ))}
-      </ul>
+      {SideBarData.map((val, key) => (
+        <div>
+          {val.title === "Crypto Sherk" ? (
+            <div className='SherkGrid'>
+            <div className="icon">{val.icon}</div>
+              <div key={key} id="sherkTitle">
+                {" "}
+                {val.title}{" "}
+              </div>
+            </div>
+          ) : (
+        <ul>
+            <li
+              key={key}
+              onClick={() => {
+                window.location.pathname = val.link;
+              }}
+              id={window.location.pathname === val.link ? "active" : ""}
+            >
+              <div className="icon">{val.icon}</div>
+
+              <div className="title">{val.title}</div>
+            </li>
+        </ul>
+          )}
+        </div>
+      ))}
     </div>
   );
-};
+}
 
 export default SideBar;
